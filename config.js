21c7b1f27c8f61301aa09a40db3270261e814cd0
Viana Ferguson <vianaferguson@yahoo.com>
2023-12-05 20:09:46 -0700

    yml file

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    OPENAI_API_KEY: "OPENAI_API_KEY"
+};
+
+// If you're working in a browser environment, you can expose the config object globally
+window.config = config;
+
+export default config;
